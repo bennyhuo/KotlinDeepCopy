c584745019285c369371ff2b4b3090548091a0d8
Bennyhuo <305779913@qq.com>
2019-05-18 15:28:25 +0800

    Update Recursive.kt

--- apt-impl/sample/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/sample/recursive/Recursive.kt
+++ apt-impl/sample/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/sample/recursive/Recursive.kt
@@ -3,13 +3,16 @@ package com.bennyhuo.kotlin.deepcopy.sample.recursive
 import com.bennyhuo.kotlin.deepcopy.annotations.DeepCopy
 
 /**
- * Uncomment owner will lead to apt exception.
+ * It will lead to an apt exception to uncomment this.
  * CopyLoopException: Detect infinite copy loop.
  * It will cause stack overflow to call com.bennyhuo.kotlin.deepcopy.sample.recursive.Owner.deepCopy() in the runtime.
  * */
 //@DeepCopy
 //data class Project(val name: String, var owner: Owner?)
 
+/**
+ * May this is what you want.
+ **/
 @DeepCopy
 data class Project(val name: String){
     lateinit var owner: Owner
