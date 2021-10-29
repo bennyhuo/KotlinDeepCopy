6b0c6f3be4bd3b8e9f05096d840d8356baf29af8
huobingqian <huobingqian@yuanfudao.com>
2021-10-29 20:45:03 +0800

    Add library.

---
+++ ksp-impl/library/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/library/PrebuiltClass.kt
@@ -0,0 +1,14 @@
+package com.bennyhuo.kotlin.deepcopy.library
+
+import com.bennyhuo.kotlin.deepcopy.annotations.DeepCopy
+import com.bennyhuo.kotlin.deepcopy.annotations.DeepCopyConfig
+
+@DeepCopy
+data class Hello<out P1, out P2>(
+    val first: P1,
+    val second: P2
+)
+
+
+@DeepCopyConfig(values = [Pair::class, Triple::class])
+class Config
