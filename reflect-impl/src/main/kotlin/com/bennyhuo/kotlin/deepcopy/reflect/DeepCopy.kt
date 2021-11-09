eb37bafeed400f104547a2b5021671a5c1942629
bennyhuo <bennyhuo@kotliner.cn>
2021-11-09 21:53:10 +0800

    delete unused code.

--- reflect-impl/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/reflect/DeepCopy.kt
+++ reflect-impl/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/reflect/DeepCopy.kt
@@ -4,21 +4,6 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.primaryConstructor
 
-//fun <T : Any> T.deepCopy(): T {
-//    if (!this::class.isData) {
-//        return this
-//    }
-//
-//    return this::class.primaryConstructor!!.let { primaryConstructor ->
-//        primaryConstructor.parameters.associateWith { parameter ->
-//            (this::class as KClass<T>).declaredMemberProperties
-//                .first { it.name == parameter.name }
-//                .get(this)
-//                ?.deepCopy()
-//        }.let(primaryConstructor::callBy)
-//    }
-//}
-
 interface DeepCopyable
 
 fun <T : DeepCopyable> T.deepCopy(): T {
