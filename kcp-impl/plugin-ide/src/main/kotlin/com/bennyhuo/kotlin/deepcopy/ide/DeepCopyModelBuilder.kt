ae7f8aa5ba9088906e2399016d66a46c395735ed
bennyhuo <bennyhuo@kotliner.cn>
2022-09-14 09:33:37 +0800

    Fix compatible issues.

--- kcp-impl/plugin-ide/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/ide/DeepCopyModelBuilder.kt
+++ kcp-impl/plugin-ide/src/main/kotlin/com/bennyhuo/kotlin/deepcopy/ide/DeepCopyModelBuilder.kt
@@ -2,15 +2,14 @@ package com.bennyhuo.kotlin.deepcopy.ide
 
 import com.bennyhuo.kotlin.kcp.BuildConfig
 import org.gradle.api.Project
-import org.jetbrains.kotlin.idea.gradleTooling.AbstractKotlinGradleModelBuilder
 import org.jetbrains.plugins.gradle.tooling.ErrorMessageBuilder
+import org.jetbrains.plugins.gradle.tooling.ModelBuilderService
 import java.io.Serializable
-import java.lang.Exception
 
 /**
  * Created by benny at 2022/1/13 8:14 AM.
  */
-class DeepCopyModelBuilder : AbstractKotlinGradleModelBuilder() {
+class DeepCopyModelBuilder : ModelBuilderService {
     override fun buildAll(modelName: String?, project: Project): Any {
         val deepCopyPlugin = project.plugins.findPlugin(BuildConfig.KOTLIN_PLUGIN_ID)
         return DeepCopyGradleModelImpl(deepCopyPlugin != null)
